8b2b3f7a3e485d4a695b4fe211e8bf38b489b859
Wolfgang Barth <wob@swobspace.net>
2014-06-28 17:54:11 +0200

    bundle update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ Dotenv.load( File.expand_path(__FILE__ + '/../.localenv'),
               File.expand_path(__FILE__ + '/../.env'))
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
+  # config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   # config.filter_run :focus
   config.filter_run_excluding :modify => true

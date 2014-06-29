900a7a52264cb7f9087a90b5e9231b61b5fc9535
Wolfgang Barth <wob@swobspace.net>
2014-06-29 13:11:37 +0200

    check created ticket for customer_user_id and customer_id
      customer_user must be found in the local customer user database

--- spec/support/variables.rb
+++ spec/support/variables.rb
@@ -4,6 +4,8 @@ shared_context "basic variables" do
   let(:otrs_user) { ENV['OTRS_USER'] }
   let(:otrs_passwd) { ENV['OTRS_PASSWD'] }
   let(:ticket_get_id) { ENV['TICKET_GET_ID'] }
+  let(:customer_user) { ENV['CUSTOMER_USER'] }
+  let(:customer_id) { ENV['CUSTOMER_ID'] }
   let(:basic_auth) {
     if ENV['HTTP_AUTH_USER']
       {basic_auth: [ENV['HTTP_AUTH_USER'], ENV['HTTP_AUTH_PASSWD']]}
--- spec/ticket_create_spec.rb
+++ spec/ticket_create_spec.rb
@@ -16,8 +16,9 @@ describe 'TicketCreate' do
     it "ticket_create returns some content" do
       subject  = "Testticket from webservice"
       text     = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+      options = { "Ticket" => { "CustomerUser" => customer_user } }
 
-      response = @ticket.ticket_create(subject, text)
+      response = @ticket.ticket_create(subject, text, options)
       expect(response.body).to include(:ticket_create_response)
       ts = response.body[:ticket_create_response]
       expect(ts).to include(:ticket_id)
@@ -27,6 +28,8 @@ describe 'TicketCreate' do
       response = @ticket.ticket_get("TicketID" => ts[:ticket_id])
       expect(response.body[:ticket_get_response][:ticket]).to include(:title)
       expect(response.body[:ticket_get_response][:ticket][:title]).to eq(subject)
+      expect(response.body[:ticket_get_response][:ticket][:customer_user_id]).to eq(customer_user)
+      expect(response.body[:ticket_get_response][:ticket][:customer_id]).to eq(customer_id)
     end
     
   end
